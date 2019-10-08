4eaab48a124420e0ce33a07ccc5d896b525bbaf0
Hu Ao <hu_ao@icloud.com>
2019-10-08 19:18:29 +0800

    problem 20

---
+++ 20/main.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == '(':
+                stack.append('(')
+            elif i == '[':
+                stack.append('[')
+            elif i == '{':
+                stack.append('{')
+            elif i == ')':
+                if len(stack) == 0 or stack[-1] != '(':
+                    return False
+                else:
+                    stack.pop()
+            elif i == ']':
+                if len(stack) == 0 or stack[-1] != '[':
+                    return False
+                else:
+                    stack.pop()
+            else:
+                if len(stack) == 0 or stack[-1] != '{':
+                    return False
+                else:
+                    stack.pop()
+        if len(stack) == 0
+            return True
+        return False
+
+sol = Solution()
+print(sol.isValid("()"))
+print(sol.isValid("()[]{}"))
+print(sol.isValid("(]"))
+print(sol.isValid("([)]"))
+print(sol.isValid("]"))
