9d65df7e60ea02fa9924b42f363ca18f9294aa0c
pauvrepetit <hu__ao@outlook.com>
2020-07-27 21:08:01 +0800

    problem 700

---
+++ 700/main.py
@@ -0,0 +1,23 @@
+# 700. 二叉搜索树中的搜索
+#
+# 20200727
+# huao
+# 二叉搜索树的查找，递归而已
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root == None:
+            return None
+        if root.val == val:
+            return root
+        if root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
