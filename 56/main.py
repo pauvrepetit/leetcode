11ac82c506bb3f4c77d0974cd7ed3d100b1ac1cc
huao <hu__ao@outlook.com>
2021-07-16 10:21:04 +0800

    problem 56

---
+++ 56/main.py
@@ -0,0 +1,24 @@
+# 56. 合并区间
+#
+# 20210716
+# huao
+
+
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        i = 0
+        while i < len(intervals) - 1:
+            if intervals[i + 1][0] <= intervals[i][1]:
+                intervals[i][1] = max(intervals[i][1], intervals[i + 1][1])
+                intervals.pop(i + 1)
+            else:
+                i += 1
+        return intervals
+
+
+print(Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+print(Solution().merge([[1, 4], [4, 5]]))
