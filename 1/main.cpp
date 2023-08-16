d95a03e8e28bf43a4fc5f324d8393bd4a85828aa
Hu Ao <hu__ao@outlook.com>
2023-08-16 15:52:35 +0800

    problem 1

---
+++ 1/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+#include <bits/stdc++.h>
+using std::vector;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res(2);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
